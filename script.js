1f83591e8fa07bbc751db725edd4db7aef8d3f3c
akiyama <iwanttob2.7lieve@gmail.com>
2024-11-25 02:54:16 +0300

    add settings
    add functionality for color picker and erase button

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 const field = document.querySelector(".drawing-field");
+const colorPicker = document.querySelector("#colorPicker")
 
 for (i = 0; i < 256; i++) {
     const fieldBlock = document.createElement("div");
     fieldBlock.setAttribute("class", "field-block");
     fieldBlock.addEventListener("mouseenter", () => {
-        fieldBlock.style.backgroundColor = 'black'
+        if (mouseDown) {
+            fieldBlock.style.backgroundColor = colorPicker.value
+        }
     });
     field.appendChild(fieldBlock);
 }
+
+const clearButton = document.querySelector(".clear")
+clearButton.addEventListener("click", () => {
+    const fieldBloks = document.querySelectorAll(".field-block");
+    for (i = 0; i < fieldBloks.length; i++) {
+        fieldBloks[i].style.backgroundColor = 'antiquewhite';
+    }
+})
